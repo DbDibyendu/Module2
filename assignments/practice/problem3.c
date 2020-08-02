8f5422fb480515b6aa23cfed7f5a15669745f5e6
Dibyendu Biswas <dbdibyendu5@gmail.com>
2020-08-02 13:48:20 +0200

    Update problem3.c

--- assignments/practice/problem3.c
+++ assignments/practice/problem3.c
@@ -119,7 +119,7 @@ struct data *parse(struct data *ptr)                                    // colle
     fp=fopen(filename,type);                                                         // or else opening the file in read mode                               
     while(Fgets(temp1,80,fp)!=NULL)                                     // by using fgets library function accepting the file content line by line and storing it into a temp1 char array 
     {
-        nu=calloc(1,sizeof(struct data));                                    // allocating memory for new node with sizeof structure  
+        nu=(struct data*)calloc(1,sizeof(struct data));                                    // allocating memory for new node with sizeof structure  
                                     
         strcpy(nu->sample,temp1);                                     //by using strcpy string copy, copying the content of temp1 to new node. 
         if(ptr==NULL) // checking condition if ptr is NULL the list is empty so, we allocate a new node to ptr initial one.
